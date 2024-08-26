62793571e15602838176d9891fd0a4265050ff84
maria mercedes florez <maria.m.florez@gmail.com>
2024-08-26 18:41:27 -0500

    alert-new-mmfr

---
+++ src/app/protected/new-alert/new-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-alert.component.html',
+  styleUrl: './new-alert.component.css'
+})
+export class NewAlertComponent {
+
+}
